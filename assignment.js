8f33c248d8d518df257cd425d284a21d41badd1d
Rachel Le <121990754+le-rachel@users.noreply.github.com>
2026-08-08 23:22:29 -0500

    Refactor assignment.js to include score calculation
    Removed HTML structure and added JavaScript function to calculate scores based on user input.

--- assignment.js
+++ assignment.js
@@ -1,56 +1,25 @@
-<!DOCTYPE html>
-<html>
+var grade = document.getElementById("grade");
+var submitAssignment = document.getElementById("submitAssignment");
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Assignment/Exam Grade</title>
+submitAssignment.addEventListener("click", calculateScore);
 
-  <!-- link the webpage's stylesheet -->
-  <link rel="stylesheet" href="/style.css" />
-
-  <!-- link the webpage's JavaScript file -->
-  <script src="/assignment.js" defer></script>
-</head>
-
-<body>
-
-  <main>
-
-    <nav>
-      <ul class="nav">
-         <li class="nav-li"><a href="index.html">Home</a></li>
-        <li class="nav-li"><a href="faqs.html">FAQS</a></li>
-      </ul>
-    </nav>
-
-    <header>
-      <h1>Assignment/Exam Grade</h1>
-    </header>
-
-    <label for="numCorrect">Amount Correct: </label>
-    <input type="number" id="numCorrect" name="numCorrect"> </br>
-    <label for="totalQuestions">Total Questions: </label>
-    <input type="number" id="totalQuestions" name="totalQuestions"> </br>
-    <label for="halfPoints">Add Half Points Back? </label> 
-    <input type="checkbox" id="halfPoints" name="halfPoints"> </br>
-    <label for="squareRoot">Add Square Root Curve? </label>
-    <input type="checkbox" id="squareRoot" name="squareRoot"> </br>
-    <label for="extraPoints">Add Extra Points? (Input 0 if N/A)</label> </br>
-    <input type="number" id="extraPoints" name="extraPoints"> </br>
-    <button class="submitbtn" id="submitAssignment">Calculate!</button>
-
-    <p class="display" id="grade">Your Grade Is...</p>
-
-    <footer>
-      <p>Made By Rachel Le</p>
-    </footer>
-    
-  </main>
-
-
-  
-</body>
-
-</html>
+function calculateScore() {
+  var numCorrect = document.getElementById("numCorrect").value;
+  var totalQuestions = document.getElementById("totalQuestions").value;
+  var squareRoot = document.getElementById("squareRoot");
+  var halfPoints = document.getElementById("halfPoints");
+  var extraPoints = document.getElementById("extraPoints").value;
+  var score = (numCorrect / totalQuestions) * 100;
+  if (halfPoints.checked) {
+    var extra = 100 - score;
+    score += (extra / 2);
+  }
+  if (squareRoot.checked) {
+    score = Math.sqrt(score);
+    score *= 10;
+  }
+  if (extraPoints != 0) {
+    score += Number(extraPoints);
+  }
+  grade.innerHTML = parseFloat(score).toFixed(2);
+}
